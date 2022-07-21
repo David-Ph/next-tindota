3344863af20a0ee91f7c1e04d660f977f3aacd9c
David <davidphang431@gmail.com>
2022-07-21 11:39:03 +0700

    Initial Setup with styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
+import { ThemeProvider } from '@mui/material/styles';
+import MainTheme from "../styles/MainTheme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={MainTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
---
+++ pages/players/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./index.module.css";
+
+function index() {
+  return (
+    <div className={styles['my-title']}>index</div>
+  )
+}
+
+export default index
