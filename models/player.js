a4ea0f8bf491dc8a5c3f044c32d03c6367378366
David <davidphang431@gmail.com>
2022-07-25 10:37:00 +0700

    Add player models

---
+++ models/player.js
@@ -0,0 +1,67 @@
+import { Schema, model, models } from "mongoose";
+
+const playerSchema = new Schema({
+  accountId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  realMmr: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  inhouseMmr: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  calibrationMmr: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  totalGames: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  totalWins: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  totalLoses: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  calibrationGames: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  calibrationWins: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  calibrationLoses: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  isCalibrated: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+const Player = models.Player || model("Player", playerSchema);
+
+export default Player;
