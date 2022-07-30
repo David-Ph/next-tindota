36275a364923835e9cc22b9a56e7975d6b23da25
David <davidphang431@gmail.com>
2022-07-30 13:35:31 +0700

    Section container

--- components/UI/SectionContainer/SectionContainer.js
+++ components/UI/SectionContainer/SectionContainer.js
@@ -1,5 +1,16 @@
+import Box from "@mui/material/Box";
+import styles from "./SectionContainer.module.css";
+
 function SectionContainer(props) {
-  return <div>{props.children}</div>;
+  return (
+    <Box
+      sx={{ backgroundColor: "tindota.primaryBg" }}
+      component={"div"}
+      className={styles.root}
+    >
+      {props.children}
+    </Box>
+  );
 }
 
 export default SectionContainer;
