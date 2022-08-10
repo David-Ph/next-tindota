9b88c9d2ffe951e068d7d8d5f82e3ff4337128fd
David <davidphang431@gmail.com>
2022-08-10 11:58:30 +0700

    Move content to document js

--- components/Layout/Head/index.js
+++ components/Layout/Head/index.js
@@ -8,10 +8,6 @@ function HeadTag() {
       <meta name="description" content="Dota Balancer" />
       <meta name="viewport" content="initial-scale=1, width=device-width" />
       <link rel="icon" href="/assets/images/logo.png" />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
     </Head>
   );
 }
